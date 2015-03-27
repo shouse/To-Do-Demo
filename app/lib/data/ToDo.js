c8c32644728b702c23b6444caf70c453cbcc820c
Steven <steven.m.house@gmail.com>
2015-03-27 09:16:53 -0500

    Demo JSON

--- app/lib/data/ToDo.js
+++ app/lib/data/ToDo.js
@@ -18,6 +18,9 @@ exports.getTasks = function() {
 }
 
 var tasks = [
-    { name: "Get Hired with Propellics", content: "Test 1", todo_id: 1},
-    { name: "Make Cool Stuff!", content: "Test 2", todo_id: 2}
+    { name: "Make a To Do App", content: "There's so many things to do... just grep a software project :)", status: 1, todo_id: 1},
+    { name: "Get your dream job!", content: "Test 2", todo_id: 2},
+    { name: "Make lots of cool stuff.", content: "Take a look at my resume!", status: 1, todo_id: 3},
+    { name: "Make lots MORE cool stuff.", content: "We all want to do this... and if you don't you have a 94% you are not currently reading this.!", status: 0},
+    { name: "Profit!", content: "Smells like the beach and tastes like excellence.", status: 0, todo_id: 4},
 ];
